22d3934e7d70b6a7542389f58649d3340bd6f15d
CatherineF101 <CatherineFernandez1996@gmail.com>
2017-09-26 15:49:20 -0400

    updated

---
+++ New Unity Project/Assets/scripts/spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+
+	public GameObject go;
+	public float StartAfter = 1f;
+
+	public float SpawnFrequency = .5f;
+
+	//Start spawning after ...seconds
+	// Use this for initialization
+	void Start () {
+		// 1: what you want to repeat as a function
+		//2: How many seconds do you want to start doing it?
+		// 3: repeat every x seconds
+		InvokeRepeating ("SpawnThing", StartAfter, SpawnFrequency);
+		
+	}
+	
+	// Update is called once per frame
+	void SpawnThing () {
+
+		transform.position = new Vector3(Random.Range(-5,5), 3, Random.Range(-5,5));
+
+		Instantiate (go, transform.position, Quaternion.identity);
+
+
+		// print the log of spawner object
+		Debug.Log(transform.position);
+		
+	}
+}
---
+++ reading responses/thefounder.md
@@ -0,0 +1,9 @@
+
+The founder is a game about starting your own company and it allow you to think of all aspects that comes with that responsibility.
+I named my corporation Kate Korp and I made my Company about making new gadgets. They start you off by giving you a product and they make you try
+to build revinue. It also allows you to also seehow you have to get rid of competetors for your product to stand out.
+
+The part of the game I really enjoyed was when you have to hire people. It makes you really think of yourself running a company and where to hire
+people and the detail in this game is so good. It even goes down to how you negotiate peoples salary so I really enjoyed playing it and I would
+play again.
+
